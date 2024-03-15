89e7abde19165ac40d0bc87bd2ade11f823473c6
michael-1102 <michaelhanley0806@gmail.com>
2024-03-15 16:11:08 -0400

    Create lifesynchub.html

--- server.js
+++ server.js
@@ -1,1 +1,94 @@
-console.log('test');
+const http = require('http');
+const fs = require('fs');
+const { url } = require('inspector');
+const port = 8000;
+let html;
+try {
+    html = fs.readFileSync('lifesynchub.html', 'utf8');
+} catch (error) {
+    throw error;
+}
+
+let pass = "";
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+readline.question('Enter password: ', pass => { // read password
+    const mysql = require("mysql2");
+    const dBCon = mysql.createConnection({ // MySQL database
+        host: "localhost",
+        user: "root",
+        database: "lifesynchub",
+        password: pass
+    });
+    dBCon.connect(function(err) { if (err) throw err; });
+    server.listen(port);
+    console.log('Listening on port ' + port + '...');
+
+});
+
+const server = http.createServer((req, res) => {
+    let urlParts = [];
+    let segments = req.url.split('/');
+    for (i = 0, num = segments.length; i < num; i++) {
+      if (segments[i] !== "") { // check for trailing "/" or double "//"
+        urlParts.push(segments[i]);
+      }
+    }
+    let resMsg = {}, body = '';
+    req.on('data', function (data) {
+      body += data;
+      if (body.length > 1e6) {
+        res.writeHead(413); // 413 payload too large
+        res.write("Payload too large.");
+        res.end();
+        req.destroy();
+      }
+    });
+    req.on('end', function () {
+        switch(req.method) {
+            case 'GET':
+                if (urlParts[0]) {
+                    switch(urlParts[0]) {
+                        case 'product-catalog':
+                            resMsg = productCatalog(req, res, urlParts);
+                            break;
+                        default:
+                            break;
+                    }
+                } else {
+                    resMsg.code = 200;
+                    resMsg.hdrs = {"Content-Type" : "text/html"};
+                    resMsg.body = html;
+                }
+                break;
+            default:
+                break;
+        }
+        if (!resMsg.code) {
+            resMsg.code = 404;
+            resMsg.hdrs = {"Content-Type" : "text/html"};
+            resMsg.body = "404 Not Found";
+        }
+        res.writeHead(resMsg.code, resMsg.hdrs);
+        res.end(resMsg.body);
+    });
+});
+
+server.once('error', function(err) {
+    if (err.code === 'EADDRINUSE') {
+      console.log('Port ' + port + ' is already in use. Please kill all processes associated with this port before launching this server.');
+      process.exit();
+    }
+});
+
+function productCatalog(req, res, urlParts) {
+    resMsg = {};
+    resMsg.code = 200;
+    resMsg.hdrs = {"Content-Type" : "text/html"};
+    resMsg.body = "catalog";
+    return resMsg;
+}
