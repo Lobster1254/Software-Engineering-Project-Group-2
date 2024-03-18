bb98abba07c053d007ea165a7c0d7032033c70f9
michael-1102 <michaelhanley0806@gmail.com>
2024-03-17 21:00:12 -0400

    Added API request to view reviews.

--- server.js
+++ server.js
@@ -92,7 +92,89 @@ server.once('error', function(err) {
     }
 });
 
-function failedDB() {
+const getProductReviews = async(product_ID) => {
+    let reviewInfo = [];
+    let reviewQuery = "select user_ID, score, description, created from productreviews where product_ID = '" + product_ID + "'";
+    await dBCon.promise().query(reviewQuery).then(([ result ]) => {
+        if (result[0]) {
+            let sum = 0;
+            for (i = 0; i < result.length; i++)
+                sum = sum + result[i].score;
+            reviewInfo[0] = sum/result.length;
+            reviewInfo[1] = result;
+        }
+    }).catch(error => {
+        reviewInfo = "Failed to load reviews.";
+    });
+    return reviewInfo;
+}
+
+const getProductInfo = async(product_ID) => {
+    let productQuery = "select * from products where product_ID = '" + product_ID + "'";
+    let discountQuery = "select d.* from discounts d, discountedproducts p where ((d.discount_ID = p.discount_ID and p.product_ID = '" + product_ID + "' and d.scope = 'product_list') or (d.category = (select category from products where product_ID = '" + product_ID + "') and d.scope = 'category')) and d.end_date >= CURDATE()";
+    let user_ID = getUserID();
+    let ordersQuery = "select o.order_ID, o.date_made, p.quantity, o.status from orders o, orderproducts p where o.order_ID = p.order_ID and user_ID = '" + user_ID + "' and p.product_ID = '" + product_ID + "'";
+    let resMsg = {};
+    let isProduct = true;
+    foundProduct = true;
+    await dBCon.promise().query(productQuery).then(([ result ]) => {
+        if (result[0]) {
+            resMsg.body = result[0];
+        } else {
+            isProduct = false;
+        }
+    }).catch(error => {
+        foundProduct = false;
+        resMsg = failedDB();
+    });
+    if (!isProduct || !foundProduct)
+        return resMsg;
+    await dBCon.promise().query(discountQuery).then(([ result ]) => {
+        if (result[0]) {
+            let set_price = resMsg.body.price;
+            let lowered_price = resMsg.body.price;
+            for (i = 0; i < result.length; i++) {
+                if (result[i].type == "set_price") {
+                    if (result[i].set_price != null && result[i].set_price < set_price)
+                        set_price = result[i].set_price;
+                } else {
+                    if (result[i].percent_off != null && result[i].percent_off <= 100 && result[i].percent_off > 0)
+                        lowered_price = lowered_price * (100-result[i].percent_off)/100;
+                }
+            }
+            if (set_price < resMsg.body.price) 
+                resMsg.body.discounted_price = set_price;
+            else 
+                resMsg.body.discounted_price = lowered_price;
+            resMsg.body.discounted_price = roundPrice(resMsg.body.discounted_price);
+            resMsg.body.discounts = result;
+        }
+    }).catch(error => {
+        resMsg.body.discounts = "Failed to load discounts.";
+    });
+    await dBCon.promise().query(ordersQuery).then(([ result ]) => {
+        if (result[0]) {
+            resMsg.body.orders = result;
+        }
+    }).catch(error => {
+        resMsg.body.reviews = "Failed to load orders.";
+    });
+    let reviewInfo = await getProductReviews(product_ID);
+    if (reviewInfo) {
+        if (reviewInfo instanceof String) {
+            resMsg.body.reviews = reviewInfo;
+        } else {
+            resMsg.body.average_rating = reviewInfo[0];
+            resMsg.body.reviews = reviewInfo[1];
+        }
+    }
+    resMsg.code = 200;
+    resMsg.hdrs = {"Content-Type" : "application/json"};
+    resMsg.body = JSON.stringify(resMsg.body);
+    return resMsg;
+}
+
+function failedDB() { // can be called when the server fails to connect to the database and that failure is fatal to the use case's function
     resMsg = {};
     resMsg.code = 503;
     resMsg.hdrs = {"Content-Type" : "text/html"};
@@ -101,84 +183,51 @@ function failedDB() {
 }
 
 async function productCatalog(req, res, urlParts) {
-    let resMsg = {};
     if (urlParts[1]) {
         let product_ID = urlParts[1];
-        let productQuery = "select * from products where product_ID = '" + product_ID + "'";
-        let reviewQuery = "select user_ID, score, description, created from productreviews where product_ID = '" + product_ID + "'";
-        let discountQuery = "select d.* from discounts d, discountedproducts p where ((d.discount_ID = p.discount_ID and p.product_ID = '" + product_ID + "' and d.scope = 'product_list') or (d.category = (select category from products where product_ID = '" + product_ID + "') and d.scope = 'category')) and d.end_date >= CURDATE()";
-        let user_ID = getUserID();
-        let ordersQuery = "select o.order_ID, o.date_made, p.quantity, o.status from orders o, orderproducts p where o.order_ID = p.order_ID and user_ID = '" + user_ID + "' and p.product_ID = '" + product_ID + "'";
-        const getProductInfo = async() => {
-            let resMsg = {};
-            await dBCon.promise().query(productQuery).then(([ result ]) => {
-                if (result[0]) {
-                    resMsg.body = result[0];
-                }
-            }).catch(error => {
-                resMsg = failedDB();
-            });
-            await dBCon.promise().query(discountQuery).then(([ result ]) => {
-                if (result[0]) {
-                    let set_price = resMsg.body.price;
-                    let lowered_price = resMsg.body.price;
-                    for (i = 0; i < result.length; i++) {
-                        if (result[i].type == "set_price") {
-                            if (result[i].set_price != null && result[i].set_price < set_price)
-                                set_price = result[i].set_price;
-                        } else {
-                            if (result[i].percent_off != null && result[i].percent_off <= 100 && result[i].percent_off > 0)
-                                lowered_price = lowered_price * (100-result[i].percent_off)/100;
-                        }
-                    }
-                    if (set_price < resMsg.body.price) 
-                        resMsg.body.discounted_price = set_price;
-                    else 
-                        resMsg.body.discounted_price = lowered_price;
-                    resMsg.body.discounted_price = roundPrice(resMsg.body.discounted_price);
-                    resMsg.body.discounts = result;
-                }
-            }).catch(error => {
-                resMsg.body.discounts = "Failed to load discounts.";
-                console.log(error);
-            });
-            await dBCon.promise().query(ordersQuery).then(([ result ]) => {
-                if (result[0]) {
-                    resMsg.body.orders = result;
-                }
-            }).catch(error => {
-                resMsg.body.reviews = "Failed to load orders.";
-                console.log(error);
-            });
-            await dBCon.promise().query(reviewQuery).then(([ result ]) => {
-                if (result[0]) {
-                    let sum = 0;
-                    for (i = 0; i < result.length; i++)
-                        sum = sum + result[i].score;
-                    resMsg.body.average_rating = sum/result.length;
-                    resMsg.body.reviews = result;
-                }
-            }).catch(error => {
-                resMsg.body.reviews = "Failed to load reviews.";
-            });
-            resMsg.code = 200;
-            resMsg.hdrs = {"Content-Type" : "application/json"};
-            resMsg.body = JSON.stringify(resMsg.body);
-            return resMsg;
-        }
-        return getProductInfo();
+        
+        return await getProductInfo(product_ID);
     } else {
-        return resMsg;
+        return {};
     }
     
 }
 
+
 async function productReviews(req, res, urlParts) {
-    let resMsg = {};
-    resMsg.code = 200;
-    resMsg.hdrs = {"Content-Type" : "text/html"};
-    resMsg.body = "reviews";
-    return resMsg;
+    if (urlParts[1]) {
+        let resMsg = {};
+        let product_ID = urlParts[1];
+        let isProduct = true;
+        let foundProduct = true;
+        await dBCon.promise().query("select product_ID from products where product_ID = '" + product_ID + "'").then(([ result ]) => {
+            console.log(result[0]);
+            if (!result[0])
+                isProduct = false;
+        }).catch(error => {
+            foundProduct = false;
+            resMsg = failedDB();
+        });
+        console.log(isProduct);
+        if (!isProduct || !foundProduct)
+            return resMsg;
+        let reviewInfo = await getProductReviews(product_ID);
+        if (reviewInfo) {
+            if (reviewInfo instanceof String) {
+                return failedDB();
+            } else {
+                resMsg.body = {};
+                resMsg.body.average_rating = reviewInfo[0];
+                resMsg.body.reviews = reviewInfo[1];
+            }
+        }
+        resMsg.code = 200;
+        resMsg.hdrs = {"Content-Type" : "application/json"};
+        resMsg.body = JSON.stringify(resMsg.body);
+        return resMsg;
+    } else {
+        return {};
+    }
 } 
 
 function getUserID() {
