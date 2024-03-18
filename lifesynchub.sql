7227c758e7e34691bf05e041ac4d6a774752fc50
michael-1102 <michaelhanley0806@gmail.com>
2024-03-18 01:39:28 -0400

    Changed helpfulness rating from int to boolean.

--- lifesynchub.sql
+++ lifesynchub.sql
@@ -60,7 +60,7 @@ DROP TABLE IF EXISTS `helpfulnessratings`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `helpfulnessratings` (
-  `rating` int NOT NULL,
+  `rating` boolean NOT NULL,
   `user_ID` varchar(50) NOT NULL,
   `review_user_ID` varchar(50) NOT NULL,
   `product_ID` varchar(50) NOT NULL,
