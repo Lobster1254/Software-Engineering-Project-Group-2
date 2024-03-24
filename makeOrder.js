2f104ec3c7a4831960d6e23bd0f4ed75be9e110a
Maya-Kalapatapu <124222148+Maya-Kalapatapu@users.noreply.github.com>
2024-03-24 15:28:53 -0400

    Update makeOrder.js
    - implement paymentIntents through Stripe API
    - discounts applied

--- makeOrder.js
+++ makeOrder.js
@@ -1,28 +1,26 @@
-//This file is for Make Order 
-/*
-    to trigger makeOrder, 
-        1. start server
-        2. on Postman: send an http "POST" request to 'localhost.../make-order/{user_ID}'
-        3. it will send response notifying you that order has been placed
-
-const http = require('http'); // for using stripe later....
-const api_key = 'YOUR_API_KEY_HERE';
-const stripe = require('stripe')(api_key);
+//This file is for Make Order (Use case 7)
               
 /* NOTE:
 "orderInfoArray" format: 
 [[{shoppingcartproducts of user}], [{cartInf0}], [{product info}]];
 */
-async function createOrder(orderInfoArray) {
+const http = require('http'); // for using stripe later....
+const api_key= 'sk_test_51OvkRMLzLOmc1QtSZk44CqTryArf3YS6PfdSDEGLdURC2QYZ2iRMF8B3x1qMzrBO51drq0vh65bp3KOq7OPZrVMa003jOO32Pe';
+const stripe = require('stripe')(api_key);
+const { validateAddress } = require('../src/validateAddresses.js');
+const { getDiscounts } = require('../server.js');
+
+async function createOrder(req, body, orderInfoArray, discounted_price) {
+    body = JSON.parse(body);
     let orderProducts = {}; //for updating the orderProducts table as result of the new order -- will be implemented later
     let order = {};
-    let body = "";
+    //let body = "";
     let shoppingCart = orderInfoArray[0];
     let shoppingCartProducts = orderInfoArray[1];
     let products = orderInfoArray[2];
     const validCart = await enoughStock(shoppingCartProducts, products);
     if(validCart) {
-        order = await makeOrder(shoppingCart, billingInfo);
+        order = await makeOrder(req, body, shoppingCart, shoppingCartProducts, discounted_price);
         console.log(order); // for testing (to see what's being returned as "order")
         body = order;
     } else {
@@ -33,13 +31,10 @@ async function createOrder(orderInfoArray) {
 
 //function checks if there are products in cart & if the requested quantity is in stock
 async function enoughStock(shoppingCartProducts, products) {
-    if(shoppingCartProducts[i].quantity > 50) {
-        return false; //there are too many products in your cart!
-    }
     let outOfStockCount = 0;
     if(shoppingCartProducts) {
         for(let i = 0; i < products.length; i++) {
-            if(products[i].stock < shoppingCartProducts[i].quantity) {
+            if(products[i].stock < shoppingCartProducts[i].quantity || shoppingCartProducts[i].quantity > 50) {
                 outOfStockCount++;
             }  
         }
@@ -51,28 +46,72 @@ async function enoughStock(shoppingCartProducts, products) {
     }
 }
 
-//for testing purposes before stripe is implemented
-let billingInfo = {
-    payment_method: 'pm_card_visa',
-    shipping_address: '76 Pollard rd, Mountain Lakes, NJ',
-    billing_address: '76 Pollard rd, Mountain Lakes, NJ'
-}
-
 //create order with given info (to be inserted into orders table)
-async function makeOrder(shoppingCart, billingInfo) {
+async function makeOrder(req, body, shoppingCart, shoppingCartProduct, discounted_prices) {
+   // if(await validateAddress(body) == true) { // implement address validation hereeeeeee
+        
     let order = { 
         order_ID: await getOrder_ID(), 
         user_ID: shoppingCart[0].user_ID, 
         date_made: await getDate(), 
-        payment_method: billingInfo.payment_method, 
-        products_cost: shoppingCart[0].cost, 
-        tax_cost: parseFloat((0.07*shoppingCart[0].cost).toFixed(2)), 
+        payment_method: body.payment_method, 
+        products_cost: discounted_prices, // to run without applying discounts: shoppingCart[0].cost, 
+        tax_cost: (Math.ceil(discounted_prices * 0.066)*10)/10, //to run without applying discounts: replace "discounted_prices" with shoppingCart[0].cost
+        total_cost: discounted_prices + (Math.ceil(discounted_prices * 0.066)*10)/10, // to run without applying discounts: Math.ceil((shoppingCart[0].cost + ((shoppingCart[0].cost) * 0.066)) * 10) / 10,
         shipping_cost: 0.00, //everyone loves free shipping --- but to be fixed later for the $$$
-        delivery_address: billingInfo.shipping_address,
-        billing_address: billingInfo.billing_address,
+        delivery_address: JSON.stringify(body.shipping_address),
+        billing_address: await getBillingAddress(body),
         status: 'not shipped'
     };
+    
+    //get payment intent if possible  ---> this is sent to stripe 
+   // however, in current state transactions are NOT complete, we don't want to handle sensitive info through our server for security reasons at this point
+   let newPaymentIntent = controller(body, order);
+    
+    //let order = stripeCreateCheckoutSession(shoppingCart); // this line is for if we decide to use stripe checkout instead of payment intents later on...
     return order;
+
+}
+
+//stripe controller
+async function controller(req, order) {
+    let pi = await getpaymentIntent(req, order);
+    console.log(pi);
+}
+
+// stripe payment intent (for confirming billing details and processing transaction)
+async function getpaymentIntent(body, order) {
+    let taxCalculation = await calculateTax(order);
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: taxCalculation.amount_total,
+      payment_method: body.payment_method, 
+      shipping: 
+        {
+            address: JSON.parse(order.delivery_address),
+            name: body.name,
+            tracking_number: null,
+        },
+      currency: 'usd',
+      receipt_email: order.user_ID,
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
+    return paymentIntent;
+    }
+
+// apply discounts
+async function applyDiscounts(order, shoppingCartProducts) {
+    let discountedPrices = {};
+    let discounted_total_cost = 0;
+    for(let i = 0; i < shoppingCartProducts.length; i++) {
+        let discountedPrice = getDiscounts(shoppingCartProducts[i].product_ID, shoppingCartProducts[i].cost);
+        discountedPrices.push(discountedPrice);
+        discounted_total_cost += discountedPrice;
+    }
+    order.total_cost = discounted_total_cost;
+    return  order;
+
 }
 
 //get & format date for orders table
@@ -92,6 +131,33 @@ async function getOrder_ID() {
     return await getRandomUniqueInt(min, max);
 }
 
+async function getBillingAddress(body){
+    if (body.billing_address_same_as_shipping_address) {
+        return JSON.stringify(body.shipping_address);
+    } else if (!body.billing_address_same_as_shipping_address) {
+        return JSON.stringify(body.billing_address);
+    } else {
+        return;
+    }
+}
+
+async function calculateTax(order) {
+    const calculation = await stripe.tax.calculations.create({
+        currency: 'usd',
+        line_items: [
+          {
+            amount: (order.products_cost)*100, //minimum purchase is $10
+            reference: 'L1',
+          },
+        ],
+        customer_details: {
+          address: JSON.parse(order.delivery_address),
+          address_source: 'shipping',
+        },
+      });
+      return calculation;
+}
+
 //function to generate random integer between min and max (inclusive) for orders_ID
 const usedNumbers = new Set();
 async function getRandomUniqueInt(min, max) {
@@ -105,9 +171,118 @@ async function getRandomUniqueInt(min, max) {
   return randomInt;
 }
 
-//function uses Stripe API complete checkout
-async function stripeCheckout(cart) {
-// NOT COMPLETE WHATSOEVER 
+/*
+//This is the in-progress stripe checkout workflow (opted to use paymentIntents instead for demo 1 for more workflow customization (i.e. using discounts w/o subscription etc))
+async function getShippingRate(shoppingCartProducts) {
+    for(let i = 0; i < )
 }
-    
+*/
+
+/*
+//function uses Stripe API complete checkout
+async function stripeCreateCheckoutSession(cart) {
+// NOT COMPLETE WHATSOEVER 
+stripe.checkout.sessions.create({
+
+    // shipping rate(s)
+    shipping_address_collection: {
+        allowed_countries: ['US'],
+      },
+      shipping_options: [
+        {
+          shipping_rate_data: {
+            type: 'fixed_amount',
+            fixed_amount: {
+              amount: 0,
+              currency: 'usd',
+            },
+            display_name: 'Free shipping',
+            tax_behavior: 'exclusive',
+            tax_code: 'txcd_10000000',
+            delivery_estimate: {
+              minimum: {
+                unit: 'business_day',
+                value: 5,
+              },
+              maximum: {
+                unit: 'business_day',
+                value: 7,
+              },
+            },
+          },
+        },
+        {
+          shipping_rate_data: {
+            type: 'fixed_amount',
+            fixed_amount: {
+              amount: 1500,
+              currency: 'usd',
+            },
+            display_name: 'Next day air',
+            delivery_estimate: {
+              minimum: {
+                unit: 'business_day',
+                value: 1,
+              },
+              maximum: {
+                unit: 'business_day',
+                value: 1,
+              },
+            },
+          },
+        },
+      ],
+    //line item(s)
+    line_items: [
+        {
+            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+            price: cart.price,
+            quantity: cart.quantity,
+        }
+    ],
+    // custom fields
+    custom_fields: [
+        {
+          key: 'engraving',
+          label: {
+            type: 'custom',
+            custom: 'Personalized engraving',
+          },
+          type: 'text',
+        },
+      ],
+  mode: 'payment',
+  success_url: `${YOUR_DOMAIN}/success.html`,
+  cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+}).then(session => {
+  res.writeHead(303, { 'Location': session.url });
+  body = session;
+  res.end();
+  //return body;
+}).catch(error => {
+  console.error('Error creating checkout session:', error);
+  res.writeHead(500, { 'Content-Type': 'text/plain' });
+  res.end('Internal Server Error');
+});
+} 
+// Serve static files
+const filePath = './public' + (pathname === '/' ? '/index.html' : pathname);
+fs.readFile(filePath, (err, data) => {
+  if (err) {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found');
+  } else {
+    res.writeHead(200);
+    res.end(data);
+    console.log(body.payment_method);
+  }
+});
+
+   // else {
+   // res.writeHead(404, { 'Content-Type': 'text/plain' });
+   // res.end('Not Found');
+   // }
+
+
+    */
 module.exports = { createOrder };
